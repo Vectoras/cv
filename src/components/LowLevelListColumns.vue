60a79c784339895fa285a3a4c97a5ea8091b8a13
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-04-03 20:41:06 +0100

    layout styling lls list columns

--- src/components/LowLevelListColumns.vue
+++ src/components/LowLevelListColumns.vue
@@ -1,5 +1,5 @@
 <template>
-  <article class="cv-low-level-section">
+  <article class="cv-low-level-section cv-ll-list-columns">
     <ul>
       <li v-for="(item, index) in list" :key="`${index}-${item}`">
         {{ item }}
@@ -27,4 +27,19 @@
   };
 </script>
 
-<style></style>
+<style lang="less">
+  @import '../less/style_config';
+
+  .cv-ll-list-columns {
+    ul {
+      list-style-type: none;
+      display: flex;
+      flex-flow: row wrap;
+
+      li {
+        width: 25%;
+        padding: 0.5rem 1rem;
+      }
+    }
+  }
+</style>
